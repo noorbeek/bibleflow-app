e31ea74481c9a695af32ad750189ea60e0e2e666
Mike Noorbeek <mike@thecodecrowd.nl>
2022-07-24 21:29:48 +0200

    .

--- src/app/pages/Home/BibleReader/index.tsx
+++ src/app/pages/Home/BibleReader/index.tsx
@@ -2,15 +2,18 @@ import axios from 'axios';
 import React, { Fragment, useEffect, useState } from 'react';
 import { useAppStore } from 'store/global';
 import { useQuery } from '@tanstack/react-query';
+import Selectbox from 'app/components/Selectbox';
 
 export default function BibleReader(props) {
   const bibleTranslations = useAppStore.getState().bibleTranslations;
   const [readerState, setReaderState] = useState<any>({
     translation: bibleTranslations[0].id,
+    books: 66,
     book: 1,
+    chapters: 1,
     chapter: 1,
+    verses: 1,
     verse: 0,
-    bibleVerses: [],
   });
 
   let where = `translation:${readerState.translation}+and+book:${readerState.book}+and+chapter:${readerState.chapter}`;
@@ -19,6 +22,11 @@ export default function BibleReader(props) {
     ['bibleVerses'],
     async () => await axios.get(`/bibleVerses?where=` + where),
   );
+
+  function setTranslation(translation) {
+    console.warn(translation);
+    setReaderState({ translation: translation.id });
+  }
 
   //   useEffect(() => {
   //     axios.get(`/bibleVerses?where=` + where).then(response => {
@@ -30,12 +38,23 @@ export default function BibleReader(props) {
     <>
       <main className="col-span-9">
         <div className="mt-4">
+          <Selectbox
+            label="Vertaling"
+            onChange={setTranslation}
+            options={bibleTranslations.map(translation => {
+              return {
+                id: translation.id,
+                text: translation.name,
+                description: translation.abbreviation,
+              };
+            })}
+          />
           <h1 className="font-large font-bold mb-4">
             {bibleTranslations[0].name} ({bibleTranslations[0].abbreviation})
           </h1>
           <div>
             {bibleVerses?.data?.data?.response?.map(verse => (
-              <div>
+              <div key={verse.id}>
                 <sup>{verse.verse}</sup> {verse.text}
               </div>
             ))}
