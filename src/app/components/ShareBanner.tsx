b6ee395347656d4da32bee434f84eb5050477ff0
Mike Noorbeek <mike@thecodecrowd.nl>
2022-07-29 16:43:18 +0200

    .

--- src/app/components/ShareBanner.tsx
+++ src/app/components/ShareBanner.tsx
@@ -15,26 +15,25 @@ export default function ShareBanner() {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="p-2 rounded-lg bg-primary shadow-lg sm:p-3">
           <div className="flex items-center justify-between flex-wrap">
-            <div className="w-0 flex-1 flex items-center">
+            <div className="shrink w-0 flex-1 flex items-center">
               <span className="flex p-2 rounded-lg">
                 <ShareIcon className="h-6 w-6 text-white" aria-hidden="true" />
               </span>
               <p className="ml-3 font-medium text-white truncate">
-                <span className="md:hidden">Selectie</span>
                 <span className="hidden md:inline">
                   {bibleVerses.length} Bijbelverzen geselecteerd
                 </span>
               </p>
             </div>
-            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <div className="order-3 mt-0 flex-shrink-0 sm:order-2 w-auto items-center">
               <a
                 onClick={share}
                 className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium hover:bg-white"
               >
-                Deel
+                Deel selectie
               </a>
             </div>
-            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+            <div className="shrink order-4 sm:order-3 sm:ml-2">
               <button
                 type="button"
                 onClick={reset}
